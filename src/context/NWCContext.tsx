1cdda0d824f08f6228db5d300b3f24f31e5b1851
cicciord <102993057+cicciord@users.noreply.github.com>
2023-11-27 18:52:29 +0100

    useNWCEnable

--- src/context/NWCContext.tsx
+++ src/context/NWCContext.tsx
@@ -1,29 +1,60 @@
+import { webln } from "@getalby/sdk";
 import React, {
   Dispatch,
   ReactNode,
   SetStateAction,
   createContext,
   useContext,
+  useEffect,
   useState,
 } from "react";
 
 export interface NWCContextType {
   nwcUrl: string;
   setNwcUrl: Dispatch<SetStateAction<string>>;
+  pendingNwcUrl: string;
+  setPendingNwcUrl: Dispatch<SetStateAction<string>>;
+  nwcAuthUrl: string;
+  setNwcAuthUrl: Dispatch<SetStateAction<string>>;
+  nostrWebLN: webln.NostrWebLNProvider | undefined;
+  setNostrWebLN: Dispatch<SetStateAction<webln.NostrWebLNProvider | undefined>>;
 }
 
 export const defaultValues = {
   nwcUrl: "",
   setNwcUrl: () => null,
+  pendingNwcUrl: "",
+  setPendingNwcUrl: () => null,
+  nwcAuthUrl: "",
+  setNwcAuthUrl: () => null,
+  nostrWebLN: undefined,
+  setNostrWebLN: () => null,
 };
 
 const NWCContext = createContext<NWCContextType>(defaultValues);
 
 export function NWCProvider({ children }: { children: ReactNode }) {
   const [nwcUrl, setNwcUrl] = useState("");
+  const [pendingNwcUrl, setPendingNwcUrl] = useState("");
+  const [nwcAuthUrl, setNwcAuthUrl] = useState("");
+  const [nostrWebLN, setNostrWebLN] = useState<
+    webln.NostrWebLNProvider | undefined
+  >(undefined);
+  // const [balance, setBalance] = useState<number | undefined>(undefined);
 
   return (
-    <NWCContext.Provider value={{ nwcUrl, setNwcUrl }}>
+    <NWCContext.Provider
+      value={{
+        nwcUrl,
+        setNwcUrl,
+        pendingNwcUrl,
+        setPendingNwcUrl,
+        nwcAuthUrl,
+        setNwcAuthUrl,
+        nostrWebLN,
+        setNostrWebLN,
+      }}
+    >
       {children}
     </NWCContext.Provider>
   );
@@ -33,8 +64,70 @@ export function useNWCContext() {
   return useContext(NWCContext);
 }
 
+export function useNWCEnable(_nwcUrl?: string) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [error, setError] = useState<any>(undefined);
+  const { nwcUrl, setNwcUrl, setNostrWebLN } = useNWCContext();
+
+  if (_nwcUrl) setNwcUrl(_nwcUrl);
+
+  useEffect(() => {
+    if (!nwcUrl) {
+      return;
+    }
+
+    async () => {
+      try {
+        setIsLoading(true);
+
+        const _nostrWebLN = new webln.NostrWebLNProvider({
+          nostrWalletConnectUrl: nwcUrl,
+        });
+        setNostrWebLN(_nostrWebLN);
+        await _nostrWebLN.enable();
+
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        setIsError(true);
+        setError(error);
+        console.log(error);
+      }
+    };
+  }, [nwcUrl]);
+
+  return [isLoading, isError, error];
+}
+
 export function useNwcUrl() {
   const { nwcUrl, setNwcUrl } = useNWCContext();
 
   return [nwcUrl, setNwcUrl];
 }
+
+export function useConnectWithAlby() {
+  const { pendingNwcUrl, setPendingNwcUrl, nwcAuthUrl, setNwcAuthUrl } =
+    useContext(NWCContext);
+
+  useEffect(() => {
+    async function connectWithAlby() {
+      const nwc = webln.NostrWebLNProvider.withNewSecret({});
+
+      const authUrl = nwc.getAuthorizationUrl({
+        name: "SplitSats App",
+      });
+
+      setPendingNwcUrl(nwc.getNostrWalletConnectUrl(true));
+      setNwcAuthUrl(authUrl.toString());
+    }
+
+    if (pendingNwcUrl !== "" && nwcAuthUrl !== "") {
+      connectWithAlby();
+    }
+  }, []);
+
+  return [pendingNwcUrl, nwcAuthUrl];
+}
+
+export function usePayInvoice() {}
